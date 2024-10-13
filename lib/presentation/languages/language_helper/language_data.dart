517aba06728f3e9583fe2a162712acdc6d4467b4
lamnhan066 <hello@lamnhan.dev>
2024-10-14 00:23:16 +0700

    update: Cập nhật ngôn ngữ tiếng Anh

--- lib/presentation/languages/language_helper/language_data.dart
+++ lib/presentation/languages/language_helper/language_data.dart
@@ -1,7 +1,10 @@
 import 'package:language_helper/language_helper.dart';
 
+import 'languages/en.dart';
+
 part 'languages/_generated.dart';
 
 LanguageData languageData = {
   LanguageCodes.vi: analysisLanguageData,
+  LanguageCodes.en: en,
 };
---
+++ lib/presentation/languages/language_helper/languages/en.dart
@@ -0,0 +1,324 @@
+const en = <String, dynamic>{
+  ///===========================================================================
+  /// Path: ./lib/core/utils/price_utils.dart
+  ///===========================================================================
+  "@path_./lib/core/utils/price_utils.dart": '',
+  'Tối đa': 'Maximum',
+
+  ///===========================================================================
+  /// Path: ./lib/infrastructure/respositories/local_auth_repository_impl.dart
+  ///===========================================================================
+  "@path_./lib/infrastructure/respositories/local_auth_repository_impl.dart": '',
+  'Sai tên đăng nhập hoặc mật khẩu': 'Incorrect username or password',
+  'Không có thông tin đăng nhập được lưu': 'No login information is saved',
+
+  ///===========================================================================
+  /// Path: ./lib/infrastructure/respositories/backup_restore_repository_impl.dart
+  ///===========================================================================
+  "@path_./lib/infrastructure/respositories/backup_restore_repository_impl.dart": '',
+  'Lưu Bản Sao Lưu': 'Save Backup',
+  'Chọn Bản Sao Lưu': 'Select Backup',
+
+  ///===========================================================================
+  /// Path: ./lib/infrastructure/services/database_service_impl.dart
+  ///===========================================================================
+  "@path_./lib/infrastructure/services/database_service_impl.dart": '',
+  'Không thể kết nối được với máy chủ': 'Unable to connect to the server',
+
+  ///===========================================================================
+  /// Path: ./lib/domain/usecases/data_services/import_data_usecase.dart
+  ///===========================================================================
+  "@path_./lib/domain/usecases/data_services/import_data_usecase.dart": '',
+  'Đã có lỗi xảy ra khi nhập dữ liệu, vui lòng kiểm tra lại dữ liệu mẫu':
+      'An error occurred while importing data, please check the sample data',
+
+  ///===========================================================================
+  /// Path: ./lib/domain/entities/order_status.dart
+  ///===========================================================================
+  "@path_./lib/domain/entities/order_status.dart": '',
+  'Đã tạo': 'Created',
+  'Đã thanh toán': 'Paid',
+  'Đã huỷ': 'Cancelled',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/riverpod/notifiers/login_provider.dart
+  ///===========================================================================
+  "@path_./lib/presentation/riverpod/notifiers/login_provider.dart": '',
+  'Đã có lỗi không xác định!': 'An unknown error occurred!',
+  // 'Đã có lỗi không xác định!': 'An unknown error occurred!',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/home_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/home_view.dart": '',
+  'Tổng Quan': 'Overview',
+  'Đơn Hàng': 'Orders',
+  'Sản Phẩm': 'Products',
+  'Báo Cáo': 'Reports',
+  'Cài Đặt': 'Settings',
+  'Đăng Xuất': 'Logout',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/products_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/products_view.dart": '',
+  'Tìm Kiếm': 'Search',
+  'STT': 'No.',
+  'ID': 'ID',
+  'Tên': 'Name',
+  'Giá nhập': 'Purchase Price',
+  'Giá bán': 'Selling Price',
+  'Loại hàng': 'Category',
+  'Số lượng': 'Quantity',
+  'Hành động': 'Action',
+  'Thông báo': 'Notification',
+  'Bạn cần có it nhất một loại hàng để có thể thêm sản phẩm.\n\n'
+      'Bạn có muốn thêm loại hàng không?': 'You need at least one category to add a product.\n\n'
+      'Do you want to add a category?',
+  'Đồng ý': 'Agree',
+  'Huỷ': 'Cancel',
+  'Xác nhận': 'Confirm',
+  'Bạn có chắc muốn xoá sản phẩm @{name} không?': 'Are you sure you want to delete the product @{name}?',
+  // 'Đồng ý': 'Agree',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  'Bộ lọc': 'Filter',
+  'OK': 'OK',
+  // 'Huỷ': 'Cancel',  // Duplicated
+  'Sắp xếp': 'Sort',
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  'Nhập Excel': 'Import Excel',
+  // 'Nhập Excel': 'Import Excel',  // Duplicated
+  'Bạn chưa chọn tệp hoặc tệp chưa được hỗ trợ': 'You have not selected a file or the file is not supported',
+  // 'Nhập Excel': 'Import Excel',  // Duplicated
+  'Dữ liệu bạn đang chọn trống!': 'The data you selected is empty!',
+  'Ok': 'Ok',
+  // 'Nhập Excel': 'Import Excel',  // Duplicated
+  'Có @{count} sản phẩm trong dữ liệu cần nhập. '
+      'Dữ liệu mới sẽ thay thế dữ liệu cũ và không thể hoàn tác.\n\n'
+      'Bạn có muốn tiếp tục không?': 'There are @{count} products in the data to be imported. '
+      'The new data will replace the old data and cannot be undone.\n\n'
+      'Do you want to continue?',
+  // 'Đồng ý': 'Agree',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  // 'Thông báo': 'Notification',  // Duplicated
+  // 'Bạn cần có it nhất một loại hàng để có thể thêm sản phẩm.\n\n' 'Bạn có muốn thêm loại hàng không?': 'You need at least one category to add a product.\n\n' 'Do you want to add a category?',  // Duplicated
+  // 'Đồng ý': 'Agree',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  'Không': 'No',
+  'Tên tăng dần': 'Name Ascending',
+  'Tên giảm dần': 'Name Descending',
+  'Giá tăng dần': 'Price Ascending',
+  'Giá giảm dần': 'Price Descending',
+  'Số lượng tăng đần': 'Quantity Ascending',
+  'Số lượng giảm đần': 'Quantity Descending',
+  'Trở về': 'Go Back',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/login_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/login_view.dart": '',
+  'Chào mừng bạn trở lại!': 'Welcome back!',
+  'Đăng nhập': 'Login',
+  'Chào mừng bạn đã trở lại! Vui lòng đăng nhập để tiếp tục': 'Welcome back! Please log in to continue',
+  'Tên tài khoản': 'Username',
+  'Mật khẩu': 'Password',
+  'Nhớ thông tin của tôi': 'Remember me',
+  'Đăng Nhập': 'Login',
+  'Cấu hình máy chủ': 'Server Configuration',
+  'Cấu Hình Máy Chủ': 'Server Configuration',
+  'Lưu': 'Save',
+  // 'Huỷ': 'Cancel',  // Duplicated
+  'Đang tự động đăng nhập...': 'Logging in automatically...',
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/settings_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/settings_view.dart": '',
+  'Ngôn Ngữ': 'Language',
+  'Chế Độ Tối': 'Dark Mode',
+  'Số dòng mỗi trang khi phân trang': 'Rows per page during pagination',
+  'Sao Lưu và Khôi Phục': 'Backup and Restore',
+  'Sao Lưu': 'Backup',
+  'Khôi Phục': 'Restore',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/dashboard_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/dashboard_view.dart": '',
+  'Biểu đồ doanh thu theo ngày trong tháng @{month}': 'Revenue chart by day in the month of @{month}',
+  'Chi tiết 3 đơn hàng gần nhất': 'Details of the 3 most recent orders',
+  'Top 5 sản phẩm bán chạy': 'Top 5 best-selling products',
+  'Top 5 sản phẩm sắp hết hàng (số lượng < 5)': 'Top 5 products running out of stock (quantity < 5)',
+  'Tổng doanh thu trong ngày': 'Total daily revenue',
+  '@{dailyRevenue} đồng': '@{dailyRevenue} VND',
+  'Tổng số đơn hàng trong ngày': 'Total orders in a day',
+  '@{count} đơn': '@{count} orders',
+  'Tổng số sản phẩm': 'Total products',
+  '@{count} sản phẩm': '@{count} products',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/orders_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/orders_view.dart": '',
+  // 'STT': 'No.',  // Duplicated
+  'Ngày Giờ': 'Date & Time',
+  'Trạng Thái': 'Status',
+  // 'Hành động': 'Action',  // Duplicated
+  // 'Xác nhận': 'Confirm',  // Duplicated
+  'Bạn có chắc muốn xoá đơn này không?': 'Are you sure you want to delete this order?',
+  // 'Đồng ý': 'Agree',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  // 'Bộ lọc': 'Filter',  // Duplicated
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/views/report_view.dart
+  ///===========================================================================
+  "@path_./lib/presentation/views/report_view.dart": '',
+  'Sản phẩm và số lượng bán tương ứng:': 'Products and corresponding sales quantities:',
+  'Doanh thu và lợi nhuận:': 'Revenue and profit:',
+  // 'Bộ lọc': 'Filter',  // Duplicated
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/image_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/image_dialog.dart": '',
+  'Thêm Ảnh': 'Add Image',
+  'Thêm': 'Add',
+  // 'Huỷ': 'Cancel',  // Duplicated
+  'Xoá Ảnh': 'Delete Image',
+  'Bạn có chắc muốn xoá ảnh này không?': 'Are you sure you want to delete this image?',
+  'Có': 'Yes',
+  // 'Không': 'No',  // Duplicated
+  'Vui lòng chọn ảnh từ đường dẫn\ntừ máy hoặc internet':
+      'Please select an image from a path\nfrom the device or the internet',
+  'Đường dẫn': 'Path',
+  'Chọn ảnh từ máy': 'Select image from device',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/category_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/category_dialog.dart": '',
+  'Chi Tiết Loại Hàng': 'Category Details',
+  'Thêm Loại Hàng': 'Add Category',
+  'Sửa Loại Hàng': 'Edit Category',
+  // 'Xác nhận': 'Confirm',  // Duplicated
+  'Bạn có chắc muốn xoá loại hàng @{name} không?': 'Are you sure you want to delete the category @{name}?',
+  // 'Đồng ý': 'Agree',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  // 'Tên': 'Name',  // Duplicated
+  'Mô tả': 'Description',
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/order_filter_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/order_filter_dialog.dart": '',
+  'Lọc theo khoảng thời gian': 'Filter by time period',
+  'Từ': 'From',
+  'Đến': 'To',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/order_form_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/order_form_dialog.dart": '',
+  'Chọn Sản Phẩm': 'Select Product',
+  'Có sản phẩm không đủ số lượng trong kho nên không thể Sao chép.\n'
+      'Vui lòng cập nhật thêm sản phẩm để tiếp tục!': 'Some products do not have enough stock to duplicate.\n'
+      'Please update the product stock to continue!',
+  // 'Ngày Giờ': 'Date & Time',  // Duplicated
+  'Trạng thái': 'Status',
+  // 'Trạng thái': 'Status',  // Duplicated
+  // 'STT': 'No.',  // Duplicated
+  'Tên Sản Phẩm': 'Product Name',
+  'Số Lượng': 'Quantity',
+  'Đơn Giá': 'Unit Price',
+  'Thành Tiền': 'Total Price',
+  'Hành Động': 'Action',
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/product_form_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/product_form_dialog.dart": '',
+  'Mã sản phẩm': 'Product Code',
+  'Tên sản phẩm': 'Product Name',
+  // 'Giá nhập': 'Purchase Price',  // Duplicated
+  'Vui lòng chỉ nhập số': 'Please enter numbers only',
+  // 'Giá bán': 'Selling Price',  // Duplicated
+  // 'Vui lòng chỉ nhập số': 'Please enter numbers only',  // Duplicated
+  // 'Số lượng': 'Quantity',  // Duplicated
+  // 'Vui lòng chỉ nhập số': 'Please enter numbers only',  // Duplicated
+  // 'Loại hàng': 'Category',  // Duplicated
+  // 'Loại hàng': 'Category',  // Duplicated
+  'Hình ảnh': 'Image',
+  'Chưa có hình ảnh': 'No image available',
+  // 'Mô tả': 'Description',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/order_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/order_dialog.dart": '',
+  'Thông Tin Đơn': 'Order Information',
+  'Thêm Đơn': 'Add Order',
+  'Sửa Đơn': 'Edit Order',
+  'Chép Đơn': 'Duplicate Order',
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/page_chooser_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/page_chooser_dialog.dart": '',
+  'Bạn cần nhập số trang': 'You need to enter a page number',
+  'Số trang phải là số nguyên': 'The page number must be an integer',
+  'Số trang phải >= 1': 'The page number must be >= 1',
+  'Số trang phải <= @{totalPage}': 'The page number must be <= @{totalPage}',
+  'Chọn trang': 'Select page',
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/product_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/product_dialog.dart": '',
+  'Thông Tin Sản Phẩm': 'Product Information',
+  'Thêm Sản Phẩm': 'Add Product',
+  'Sửa Sản Phẩm': 'Edit Product',
+  'Chép Sản Phẩm': 'Duplicate Product',
+  // 'Xác nhận': 'Confirm',  // Duplicated
+  // 'Bạn có chắc muốn xoá sản phẩm @{name} không?': 'Are you sure you want to delete the product @{name}?',  // Duplicated
+  // 'Đồng ý': 'Agree',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+  // 'OK': 'OK',  // Duplicated
+  // 'Huỷ': 'Cancel',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/report_filter_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/report_filter_dialog.dart": '',
+  // 'Lọc theo khoảng thời gian': 'Filter by time period',  // Duplicated
+  // 'Từ': 'From',  // Duplicated
+  // 'Đến': 'To',  // Duplicated
+
+  ///===========================================================================
+  /// Path: ./lib/presentation/widgets/product_filter_dialog.dart
+  ///===========================================================================
+  "@path_./lib/presentation/widgets/product_filter_dialog.dart": '',
+  'Lọc theo mức giá': 'Filter by price range',
+  // 'Từ': 'From',  // Duplicated
+  'Không được bỏ trống': 'Cannot be empty',
+  'Phải là số nguyên': 'Must be an integer',
+  // 'Đến': 'To',  // Duplicated
+  // 'Tối đa': 'Maximum',  // Duplicated
+  // 'Không được bỏ trống': 'Cannot be empty',  // Duplicated
+  // 'Phải là số nguyên': 'Must be an integer',  // Duplicated
+  'Lọc theo loại hàng': 'Filter by category',
+  // 'Loại hàng': 'Category',  // Duplicated
+  'Tất cả': 'All',
+};
