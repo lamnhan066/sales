f627c7aff89fd1ca4a5030ca97fdc034fc9c16fb
lamnhan066 <hello@lamnhan.dev>
2024-10-15 13:47:08 +0700

    feat: Bổ sung tuỳ chọn theo tuần và tháng hiện tại cho bộ lọc ở report

---
+++ lib/domain/entities/report_date_range_type.dart
@@ -0,0 +1,1 @@
+enum ReportDateRangeType { week, month, custom }
--- lib/presentation/widgets/report_filter_dialog.dart
+++ lib/presentation/widgets/report_filter_dialog.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:language_helper/language_helper.dart';
 import 'package:sales/core/extensions/data_time_extensions.dart';
 import 'package:sales/domain/entities/ranges.dart';
+import 'package:sales/domain/entities/report_date_range_type.dart';
 
 class ReportFilterDialog extends StatefulWidget {
   const ReportFilterDialog({
@@ -21,13 +22,19 @@ class ReportFilterDialog extends StatefulWidget {
 class _ReportFilterDialogState extends State<ReportFilterDialog> {
   final startController = TextEditingController();
   final endController = TextEditingController();
-  late Ranges<DateTime> values;
+  ReportDateRangeType _dateRangeType = ReportDateRangeType.week;
+  late Ranges<DateTime> _dateRange;
 
   @override
   void initState() {
-    values = widget.initialDateRange;
-    startController.text = values.start.toddMMyyyy();
-    endController.text = values.end.toddMMyyyy();
+    _dateRange = widget.initialDateRange;
+    _dateRangeType = switch (_dateRange) {
+      WeekDaysRanges _ => ReportDateRangeType.week,
+      MonthDaysRanges _ => ReportDateRangeType.month,
+      _ => ReportDateRangeType.custom,
+    };
+    startController.text = _dateRange.start.toddMMyyyy();
+    endController.text = _dateRange.end.toddMMyyyy();
     super.initState();
   }
 
@@ -35,67 +42,108 @@ class _ReportFilterDialogState extends State<ReportFilterDialog> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Column(
-          children: [
-            Text('Lọc theo khoảng thời gian'.tr),
-            Row(
-              children: [
-                Expanded(
-                  child: BoxWInput(
-                    controller: startController,
-                    title: 'Từ'.tr,
-                    readOnly: true,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    final value = await showDatePicker(
-                      context: context,
-                      firstDate: DateTime(1990),
-                      lastDate: DateTime.now(),
-                    );
-
-                    if (value != null) {
-                      startController.text = value.toddMMyyyy();
-                      values = Ranges(value, values.end);
-                      widget.onDateRangeChanged(values);
-                    }
-                  },
-                  icon: const Icon(Icons.calendar_month_rounded),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: BoxWInput(
-                    controller: endController,
-                    title: 'Đến'.tr,
-                    readOnly: true,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () async {
-                    final value = await showDatePicker(
-                      context: context,
-                      firstDate: DateTime(1990),
-                      lastDate: DateTime.now(),
-                    );
-
-                    if (value != null) {
-                      endController.text = value.toddMMyyyy();
-                      values = values.copyWith(end: value);
-                      values = Ranges(values.start, value);
-                      widget.onDateRangeChanged(values);
-                    }
-                  },
-                  icon: const Icon(Icons.calendar_month_rounded),
-                ),
-              ],
-            ),
-          ],
+        Text(
+          'Lọc theo khoảng thời gian'.tr,
+          style: const TextStyle(fontSize: 18),
         ),
+        RadioListTile<ReportDateRangeType>(
+          title: Text('Tuần hiện tại'.tr),
+          value: ReportDateRangeType.week,
+          groupValue: _dateRangeType,
+          onChanged: (value) {
+            if (value != null) {
+              setState(() {
+                _dateRangeType = value;
+              });
+              _changeDateRange(WeekDaysRanges(DateTime.now()));
+            }
+          },
+        ),
+        RadioListTile<ReportDateRangeType>(
+          title: Text('Tháng hiện tại'.tr),
+          value: ReportDateRangeType.month,
+          groupValue: _dateRangeType,
+          onChanged: (value) {
+            if (value != null) {
+              setState(() {
+                _dateRangeType = value;
+              });
+              _changeDateRange(MonthDaysRanges(DateTime.now()));
+            }
+          },
+        ),
+        RadioListTile<ReportDateRangeType>(
+          title: Text('Tuỳ chọn'.tr),
+          value: ReportDateRangeType.custom,
+          groupValue: _dateRangeType,
+          onChanged: (value) {
+            if (value != null) {
+              setState(() {
+                _dateRangeType = value;
+              });
+            }
+          },
+        ),
+        if (_dateRangeType == ReportDateRangeType.custom) ...[
+          Row(
+            children: [
+              Expanded(
+                child: BoxWInput(
+                  controller: startController,
+                  title: 'Từ'.tr,
+                  readOnly: true,
+                ),
+              ),
+              IconButton(
+                onPressed: () async {
+                  final value = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1990),
+                    lastDate: DateTime.now(),
+                  );
+
+                  if (value != null) {
+                    _changeDateRange(Ranges(value, _dateRange.end));
+                  }
+                },
+                icon: const Icon(Icons.calendar_month_rounded),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: BoxWInput(
+                  controller: endController,
+                  title: 'Đến'.tr,
+                  readOnly: true,
+                ),
+              ),
+              IconButton(
+                onPressed: () async {
+                  final value = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1990),
+                    lastDate: DateTime.now(),
+                  );
+
+                  if (value != null) {
+                    _changeDateRange(Ranges(_dateRange.start, value));
+                  }
+                },
+                icon: const Icon(Icons.calendar_month_rounded),
+              ),
+            ],
+          )
+        ]
       ],
     );
+  }
+
+  void _changeDateRange(Ranges<DateTime> range) {
+    _dateRange = range;
+    startController.text = _dateRange.start.toddMMyyyy();
+    endController.text = _dateRange.end.toddMMyyyy();
+    widget.onDateRangeChanged(_dateRange);
   }
 }
