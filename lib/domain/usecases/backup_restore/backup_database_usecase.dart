3d840fe1304fcdd249940b01357798b7bdbc279e
lamnhan066 <hello@lamnhan.dev>
2024-11-14 14:40:59 +0700

    feat: Trả về file khi gọi backup

--- lib/domain/usecases/backup_restore/backup_database_usecase.dart
+++ lib/domain/usecases/backup_restore/backup_database_usecase.dart
@@ -1,18 +1,19 @@
+import 'dart:io';
+
 import 'package:sales/core/errors/failure.dart';
 import 'package:sales/core/usecases/usecase.dart';
 import 'package:sales/domain/entities/backup_data.dart';
 import 'package:sales/domain/repositories/backup_restore_repository.dart';
 import 'package:sales/infrastructure/exceptions/backup_exception.dart';
 
-class BackupDatabaseUseCase implements UseCase<void, BackupData> {
-
+class BackupDatabaseUseCase implements UseCase<File, BackupData> {
   const BackupDatabaseUseCase(this._repository);
   final BackupRestoreRepository _repository;
 
   @override
-  Future<void> call(BackupData params) async {
+  Future<File> call(BackupData params) async {
     try {
-      await _repository.backup(params);
+      return await _repository.backup(params);
     } on BackupException catch (e) {
       throw BackupFailure(e.message);
     }
